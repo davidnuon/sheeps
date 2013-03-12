0861f2a1d41c750ed24f65876c12a6642808dfc1
Julia Lai <jwwlai@gmail.com>
2013-03-11 21:25:55 -0700

    sheeps are cool

---
+++ baa.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void foo(string s) {
+	cout << s << "This is a nice test for all things in the world." << endl
+}
+
+int main() {
+	foo("Hello. ");
+}
