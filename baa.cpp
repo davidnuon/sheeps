11e04e25cb1a94af0d53b09e755506ed2a009d4b
David Nuon <david@lv-node64.buyvm.net>
2013-03-12 08:32:33 +0400

    You need semicolons D:<

--- baa.cpp
+++ baa.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 void foo(string s) {
-	cout << s << "This is a nice test for all things in the world." << endl
+	cout << s << "This is a nice test for all things in the world." << endl;
 }
 
 int main() {
